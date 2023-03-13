e8da1707bde68ac742ab9b2f4140cfc445fb1348
konan <abdelaziz.git@gmail.com>
2023-03-13 19:05:01 +0100

    add repository test

--- tests/CustomerIsGoldTest.php
+++ tests/CustomerIsGoldTest.php
@@ -4,6 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 use App\CustomerIsGold;
 use App\Customer;
+use App\CustomerRepository;
 
 class CustomerIsGoldTest extends TestCase
 {
@@ -18,5 +19,21 @@ class CustomerIsGoldTest extends TestCase
 
         $this->assertTrue($sprecification->isSatisfiedBy($goldCustomer));
         $this->assertFalse($sprecification->isSatisfiedBy($silverCustomer));
+    }
+
+
+    /** @test */
+    function it_fetch_all_customers_who_match_given_specifications()
+    {
+        $customers = new CustomerRepository([
+            new Customer('gold'),
+            new Customer('silver'),
+            new Customer('bronze'),
+            new Customer('gold'),
+            new Customer('silver'),
+        ]);
+        $results = $customers->bySpecification(new CustomerIsGold);
+
+        $this->assertCount(2, $results);
     }
 }
